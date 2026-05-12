8474d5e437524da845f060827f75a95b91966b91
Sanshrit Bakshi <70140828+Sanshrit@users.noreply.github.com>
2026-05-12 17:54:30 -0400

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def trav(low,high,target):
+            if low > high:
+                return -1
+            
+            mid = (low + high)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                return trav(low,mid-1,target)
+            else:
+                return trav(mid+1,high,target)
+        return trav(0,len(nums)-1,target)
