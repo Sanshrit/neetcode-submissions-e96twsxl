2ca1f58e1dd70a97a44e2a92722a7bf161f2a7eb
Sanshrit Bakshi <70140828+Sanshrit@users.noreply.github.com>
2026-06-03 00:57:47 -0400

    Add: balanced-binary-tree - submission-8

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-8.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def trav(node):
+            if not node:
+                return 0
+            lh = trav(node.left)
+            if lh == -1:
+                return -1
+            rh = trav(node.right)
+            if rh == -1:
+                return -1
+            if abs(lh-rh) > 1:
+                return -1
+            return 1 + max(rh,lh)
+        return False if trav(root)==-1 else True
+        
